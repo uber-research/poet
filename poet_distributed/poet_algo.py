d31078cd993ab9c3784c3e1676723a8f40cb5f8b
Leonaire <hexin0515@gmail.com>
2022-01-05 11:16:40 +0100

    debug

--- poet_distributed/poet_algo.py
+++ poet_distributed/poet_algo.py
@@ -354,6 +354,7 @@ class MultiESOptimizer:
                  reset_optimizer=True):
 
         for iteration in range(iterations):
+            print('Iteration:',iteration)
             self.adjust_envs_niches(iteration, self.args.adjust_interval * steps_before_transfer,
                                     max_num_envs=self.args.max_num_envs)
 
--- run_poet_local.sh
+++ run_poet_local.sh
@@ -31,5 +31,5 @@ python -u master.py \
   --adjust_interval=8 \
   --propose_with_adam \
   --steps_before_transfer=25 \
-  --num_workers 3 \
+  --num_workers 1 \
   --n_iterations=50000 2>&1 | tee ~/ipp/$experiment/run.log
