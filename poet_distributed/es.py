5fd3c012b1135781d952a95564029d1cbe2ccf4f
umair-nasir14 <68095790+umair-nasir14@users.noreply.github.com>
2022-03-23 09:52:46 +0200

    Update es.py

--- poet_distributed/es.py
+++ poet_distributed/es.py
@@ -594,10 +594,10 @@ class ESOptimizer:
 
         raw_scores = []
         for source_optim in archived_optimizers.values():
-            raw_scores.append(cap_score(self.evaluate_theta(source_optim.theta)), lower_bound, upper_bound)
+            raw_scores.append(cap_score(self.evaluate_theta(source_optim.theta), lower_bound, upper_bound))
 
         for source_optim in optimizers.values():
-            raw_scores.append(cap_score(self.evaluate_theta(source_optim.theta)), lower_bound, upper_bound)
+            raw_scores.append(cap_score(self.evaluate_theta(source_optim.theta), lower_bound, upper_bound))
 
         self.pata_ec = compute_centered_ranks(np.array(raw_scores))
 
